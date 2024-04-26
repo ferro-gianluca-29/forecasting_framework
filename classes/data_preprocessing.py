69453797fe5a988478803aef3cb75b32b0621d28
ferro-gianluca-29 <165300326+ferro-gianluca-29@users.noreply.github.com>
2024-04-26 18:19:32 +0200

    added choice between dataset split by date or by size

--- classes/data_preprocessing.py
+++ classes/data_preprocessing.py
@@ -287,29 +287,31 @@ class DataPreprocessor():
 
     def split_data(self, df):
         n = len(df)
-        if self.validation:
-            train = df[df['date'].between(self.date_list[0], self.date_list[1])]
-            valid = df[df['date'].between(self.date_list[2], self.date_list[3])]
-            test = df[df['date'].between(self.date_list[4], self.date_list[5])]
-            return train, test, valid
+        if self.date_list is not None:
+            if self.validation:
+                train = df[df['date'].between(self.date_list[0], self.date_list[1])]
+                valid = df[df['date'].between(self.date_list[2], self.date_list[3])]
+                test = df[df['date'].between(self.date_list[4], self.date_list[5])]
+                return train, test, valid
+            else:
+                train = df[df['date'].between(self.date_list[0], self.date_list[1])]
+                test = df[df['date'].between(self.date_list[2], self.date_list[3])]
+                return train, test, None 
         else:
-            train = df[self.date_list[0]:self.date_list[1]]
-            test = df[self.date_list[2]:self.date_list[3]]
-            return train, test, None 
-        if  self.validation:
-            train_end = int(n * self.train_size)
-            valid_end = int(n * (self.train_size + self.val_size))
-                                                                    
-            train = df.iloc[:train_end]
-            valid = df.iloc[train_end:valid_end] 
-            test = df.iloc[valid_end:] 
-            print(f"training: {(train.index[0],train.index[-1])} \t valid: {valid.index[0],valid.index[-1]} \t test: {test.index[0],test.index[-1]}")
+            if  self.validation:
+                train_end = int(n * self.train_size)
+                valid_end = int(n * (self.train_size + self.val_size))
+                                                                        
+                train = df.iloc[:train_end]
+                valid = df.iloc[train_end:valid_end] 
+                test = df.iloc[valid_end:] 
+                print(f"training: {(train.index[0],train.index[-1])} \t valid: {valid.index[0],valid.index[-1]} \t test: {test.index[0],test.index[-1]}")
 
-            return train, test, valid
-        else:
-            train = df[:int(n * self.train_size)]
-            test = df[int(n * self.train_size):]
-            return train, test, None 
+                return train, test, valid
+            else:
+                train = df[:int(n * self.train_size)]
+                test = df[int(n * self.train_size):]
+                return train, test, None 
         
         
 
